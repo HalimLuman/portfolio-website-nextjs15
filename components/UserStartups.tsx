6e81330c89fd02450f864a982d2b122810579d49
HalimLuman <halim.luman0@gmail.com>
2024-11-04 10:46:59 +0100

    Final version w/o detailed error and bug fixes

---
+++ components/UserStartups.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {client} from "@/sanity/lib/client";
+import {STARTUPS_BY_AUTHOR_QUERY} from "@/sanity/lib/query";
+import StartupCard, {StartupCardType} from "@/components/StartupCard";
+
+const UserStartups = async({id}: {id: string}) => {
+    const startups = await client.fetch(STARTUPS_BY_AUTHOR_QUERY,{id})
+    return (
+        <>
+            {startups.length > 0 ? startups.map((startup: StartupCardType) => <StartupCard key={startup._id} post={startup}/>) : <p>No Startups to show</p>}
+        </>
+    )
+}
+export default UserStartups
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://bbdfb15edb28f5f77c9e0ff979551cf4@o4507768657870848.ingest.de.sentry.io/4508238927233104",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
